4e1824bc1ffd173ef4c5bd538d51bac605d6bcaa
Curt Odar <curoda@users.noreply.github.com>
2023-12-24 11:00:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -202,7 +202,7 @@ document.addEventListener('DOMContentLoaded', function() {
                              "<br><br>MERRY CHRISTMAS from Curt and Eve! You can take the cash or try again...";
         } else {
             defaultImage = "taylor.jpg";
-            cdefaultMessage = "I got a blank check baby, and I'll write your name. You won: $" + amountWon.toFixed(2) + 
+            defaultMessage = "I got a blank check baby, and I'll write your name. You won: $" + amountWon.toFixed(2) + 
                                "<br><br>Total Christmas Cash: $" + totalWinnings + 
                                "<br><br>MERRY CHRISTMAS from Curt and Eve! You can take the cash or try again...";
         }
